904abf4663d66d12e315a03815b507bce059f24a
angelicacruzopf <angelica.cruz@oneparkfinancial.com>
2024-02-15 11:56:06 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-mobile": "url('./img/banner-mobile.png')",
+        "hero-desktop": "url('./src/img/banner-desktop.png')",
+      },
+
+      lineHeight: {
+        "extra-loose": "2.5",
+        12: "3rem",
+      },
+      colors: {
+        primary: "#FE860E",
+        secondary: "#D62744",
+        third: "#FF4463",
+        fourth: "#FFD10C",
+        fifth: "#FFB605",
+        sixth: "#001836",
+        textHover: "#898198",
+
+        darkGray: "#191A1B",
+        mediumGray: "#292D32",
+        neutralGray: "#666666",
+        lightGray: "#D2D2D2",
+        grayWhithe: "#F0F0F0",
+        grayText: "797C94",
+      },
+      height: {
+        100: "26rem",
+        120: "28rem",
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
